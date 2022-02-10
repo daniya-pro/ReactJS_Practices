8aa9f3c40c5312f491e736f638e837e5eb373a2d
Syeda Daniya Haider <syedadaniyahaider@gmail.com>
2022-02-10 18:44:06 +0500

    Update App.js

--- react_state/src/App.js
+++ react_state/src/App.js
@@ -14,15 +14,28 @@ class App extends Component {
     backgroundColor: "lightblue",
     textDecoration: "underline",
   };
+
+  plus=()=>{
+    const number = this.state.number;
+
+    this.setState({number:number+1})
+  }
+  minus =()=>{
+const number = this.state.number;
+
+if(number > 0) this.setState({number:number-1})
+
+  }
+
   render() {
     const number = this.state.number;
     return (
       <div>
         
         <h1> {number} </h1>
-        <button>Plus</button>
+        <button onClick={this.plus}>Plus</button>
 
-        <button>Minus</button>
+        <button onClick={this.minus}>Minus</button>
       </div>
     );
   }
