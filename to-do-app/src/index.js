ab87d19896748ff5acf1b370551cf11ad8b28072
daniya-pro <syedadaniyahaider@gmail.com>
2022-04-10 23:11:09 +0500

    Update index.js

--- to-do-app/src/index.js
+++ to-do-app/src/index.js
@@ -1,78 +1,12 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-class Header extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {favoritecolor: "yellow"};
-  }
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({favoritecolor: "pink"})
-    }, 1000)
-  }
-  render() {
-    return (
-      <h1>My Favorite Color is {this.state.favoritecolor}</h1>
-    );
-  }
-}
-class Header2 extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {favoritecolor: "red"};
-  }
-  static getDerivedStateFromProps(props, state) {
-    return {favoritecolor: props.favcol };
-  }
-  render() {
-    return (
-      <h1>My Favorite Color is {this.state.favoritecolor}</h1>
-    );
-  }
-}
-
-class Header3 extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {favoritecolor: "red"};
-  }
-
-  static getDerivedStateFromProps(props, state) {
-    return {favoritecolor: props.favcol };
-  }
-  changeColor = () => {
-    this.setState({favoritecolor: "brown"});
-  }
-  render() {
-    return (
-      <div>
-      <h1>My Favorite Color is {this.state.favoritecolor}</h1>
-      <button type="button" onClick={this.changeColor}>Change color</button>
-      </div>
-    );
-  }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
- 
-
-ReactDOM.render(<div><Header /><Header3 favcol='blue'/><Header2 favcol="yellow"/></div>, document.getElementById('root'));
-
-
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
