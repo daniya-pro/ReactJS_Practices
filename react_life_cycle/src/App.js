fbcb37c18a1937a1e0ed848b4c4aefba8ed0d5b4
daniya-pro <syedadaniyahaider@gmail.com>
2022-04-10 23:11:04 +0500

    Update App.js

--- react_life_cycle/src/App.js
+++ react_life_cycle/src/App.js
@@ -6,18 +6,40 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      number: 1,
-    };
-  }
+      number: 1,favoritecolor: "red"};
+      console.log('construtor')
+    
+    }
+    componentWillUnmount() {
+      console.log("The component named Header is about to be unmounted.");
+    }
+    componentWillMount() {
 
- 
+      console.log('component will mount')
 
+
+    }
+    componentDidMount() {
+      console.log('component did mount')
+    }
+    
+    componentDidUpdate() {
+     console.log('component did update')
+    }
+    
+  // shouldComponentUpdate() {
+  //   return false;
+  // }
   render() {
+    console.log('component is rendered')
+
     return (
       <div>
-        
-        <h1>React Life Cycle Methods </h1>
-   
+        <div id="div1"></div>
+        <div id="div2"></div>
+        <h1>{this.state.number} </h1>
+        <button onClick={()=>this.setState({number:this.state.number+1})}>+</button>
+        <button onClick={()=>this.setState({number:this.state.number-1})}>-</button>
       </div>
     );
   }
