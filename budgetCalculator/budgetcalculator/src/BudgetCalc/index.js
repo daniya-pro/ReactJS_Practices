55b90e45570a4587e2da698e9f568c39d92538bc
daniya-pro <syedadaniyahaider@gmail.com>
2022-05-13 23:56:09 +0500

    Update index.js

--- budgetCalculator/budgetcalculator/src/BudgetCalc/index.js
+++ budgetCalculator/budgetcalculator/src/BudgetCalc/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 function UiOFBudgetCalc() {
     return ( <div>
-
-
+<h1>budget Calculator</h1>
+<div><div><span>charge</span><span>amount</span></div></div>
         
     </div> );
 }
