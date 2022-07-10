69051b7a2ac01568ad66909d1db0b509030b1f1b
daniya-pro <syedadaniyahaider@gmail.com>
2022-07-11 02:50:06 +0500

    changed

--- budgetCalculator/budgetcalculator/src/BudgetCalc/index.js
+++ budgetCalculator/budgetcalculator/src/BudgetCalc/index.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React from "react";
 function UiOFBudgetCalc() {
-    return ( <div>
-<h1>budget Calculator</h1>
-<div><div><span>charge</span><span>amount</span></div></div>
-        
-    </div> );
+  return (
+    <div>
+      <h1>budget Calculator</h1>
+      <div>
+        <div>
+          <span>charge</span>
+          <span>amount</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default UiOFBudgetCalc;
--- budgetCalculator/budgetcalculator/src/components/Home/index.js
+++ budgetCalculator/budgetcalculator/src/components/Home/index.js
@@ -1,31 +1,75 @@
-import React, { useState } from 'react';
-import "./index.css"
+import React, { useState } from "react";
+import "./index.css";
 function Home() {
-    const [valueO, setValue] = useState({});
-    console.log(valueO)
-    return (
+  const [amount, setAmount] = useState('');
+  const [chargeTitle, setCharges] = useState('');
+  const [submittedData, setSubmittedData] = useState([]);
+  const [error, setError] = useState("");
+  console.log("submittedData", submittedData);
+
+  const submit = () => {
+    if (!amount)
+      return setError("Please write amount bigger than 0");
+ if(!chargeTitle.length)
+ return setError("please fill the charges input")
+
+    let previousData = submittedData || [];
+    let newData = previousData.concat([{ amount, chargeTitle }]);
+
+    setSubmittedData(newData);
+  };
+
+  return (
     <div>
-        <h1 className="color-w">
-            Budget Calculator
-      </h1><div>
-          </div> <div className="white-box">
-      <div className='theflexbox'>
+                  {error && <p className="errorText">{error}</p>}
+
+      <h1 className="color-w">Budget Calculator</h1>
+      <div></div>
+      {}
+      <div className="white-box">
+        <div className="theflexbox">
           <div className="flex space-around">
-          <div>
-                  charge
-                <br/>  <input placeholder='e.g. 100' className="inputui" onchange={(e)=>{console.log(e.target)
-                    
-                    setValue({v1:e.target.value})}}/>
-              </div><div>
-                  Amount
-                  <br/><input placeholder='e.g. 100' className="inputui" />
-              </div>
+            <div>
+              charge
+              <br />{" "}
+              <input
+                placeholder="e.g. 100"
+                className="inputui"
+                value={chargeTitle}
+                onChange={(e) => {
+                  setCharges(e.target.value);
+                }}
+              />
+            </div>
+            <div>
+              Amount
+              <br />
+              <input
+                placeholder="e.g. 100"
+                className="inputui"
+                value={amount}
+                onChange={(e) => {
+                  setAmount(parseInt(e.target.value)||'');
+                }}
+              />
+            </div>
           </div>
-         <br/> <button className="submit" onClick={()=>{}}>SUBMIT</button>
-     </div>          <br/><br/><br/>
- </div>
- <h1 className="color-w">TotalSpending:<span className="bgcolor-r">$ 0 </span></h1>
-    </div> );
+       
+          <br />{" "}
+          <button className="submit" onClick={submit}>
+            SUBMIT
+          </button>
+        </div>{" "}
+        <br />
+        <br />
+        <br />
+      </div>
+      <h1 className="color-w">
+        TotalSpending:<span className="bgcolor-r">$ 0 </span>
+      </h1>
+    </div>
+  );
 }
+//setValue({v1:e.target.value})
 // onChange={(e)=>{setValue({v1:e.value})}}
 export default Home;
