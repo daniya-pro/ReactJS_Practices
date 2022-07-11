d3ca0857c1db90a7c86f8418772ecac597b6ebd2
daniya-pro <syedadaniyahaider@gmail.com>
2022-07-12 00:25:52 +0500

    edited

--- budgetCalculator/budgetcalculator/src/components/Home/index.js
+++ budgetCalculator/budgetcalculator/src/components/Home/index.js
@@ -1,27 +1,33 @@
 import React, { useState } from "react";
 import "./index.css";
 function Home() {
-  const [amount, setAmount] = useState('');
-  const [chargeTitle, setCharges] = useState('');
+  const [amount, setAmount] = useState("");
+  const [chargeTitle, setCharges] = useState("");
   const [submittedData, setSubmittedData] = useState([]);
   const [error, setError] = useState("");
+  const [TorF,setTorF]=useState("false")
   console.log("submittedData", submittedData);
 
   const submit = () => {
-    if (!amount)
-      return setError("Please write amount bigger than 0");
- if(!chargeTitle.length)
- return setError("please fill the charges input")
+    if (!amount) return setError("Please write amount bigger than 0");
+    if (!chargeTitle.length) return setError("please fill the charges input");
 
     let previousData = submittedData || [];
     let newData = previousData.concat([{ amount, chargeTitle }]);
 
     setSubmittedData(newData);
+    console.log(submittedData,'new data')
+    setAmount("");
+    setCharges("");
   };
 
   return (
     <div>
-                  {error && <p className="errorText">{error}</p>}
+      {error && (
+        <div className="flex jcc">
+          <p className="w50 errorText p50">{error}</p>
+        </div>
+      )}
 
       <h1 className="color-w">Budget Calculator</h1>
       <div></div>
@@ -49,17 +55,40 @@ function Home() {
                 className="inputui"
                 value={amount}
                 onChange={(e) => {
-                  setAmount(parseInt(e.target.value)||'');
+                  setAmount(parseInt(e.target.value) || "");
                 }}
               />
             </div>
           </div>
-       
           <br />{" "}
           <button className="submit" onClick={submit}>
             SUBMIT
           </button>
-        </div>{" "}
+        </div>
+        <br />
+        {submittedData ? (
+          submittedData.map((e, i) => {
+            return (
+              <div className="flex center jcc">
+                <div className="flex w50 space-around p50 bb">
+                  <span className="charge">{e.chargeTitle}</span>{" "}
+                  <span className="rb wc amount">${e.amount}</span>{" "}
+                  <span>
+                    <button className="edit" onClick={()=>{
+setAmount(e.amount)
+setCharges(e.chargeTitle)
+setTorF("true")
+
+                    }}>Edit</button>
+                    <button className="delete">Delete</button>
+                  </span>
+                </div>
+              </div>
+            );
+          })
+        ) : (
+          <div></div>
+        )}{" "}
         <br />
         <br />
         <br />
@@ -71,5 +100,7 @@ function Home() {
   );
 }
 //setValue({v1:e.target.value})
+//**<div className="flex center jcc"><div className="flex w50 space-around p50 bb"><span>sp0</span> <span className="rb wc">$09</span> <span>✏️🗑</span></div></div> */
 // onChange={(e)=>{setValue({v1:e.value})}}
+//e {amount: 44, chargeTitle: 'g'}amount: 44chargeTitle: "g"[[Prototype]]: Object i 3
 export default Home;
